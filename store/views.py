baeccce4197e8618d826175bc0052a887a37bae5
Md Tanvir Reza <tanvir.reza.abc@gmail.com>
2023-07-28 05:16:45 +0600

    day 1

---
+++ store/views.py
@@ -0,0 +1,35 @@
+from django.shortcuts import render
+from .models import Category, Product
+from django.shortcuts import get_object_or_404
+
+# Create your views here.
+def index(request):
+    products = Product.objects.all()
+
+    context = {
+        'products': products
+    }
+    return render(request, 'store.html', context)
+
+def product_detail(request, slug):
+    product = get_object_or_404(Product, slug=slug)
+    context = {
+        'product': product
+    }
+    return render(request, 'product_details.html', context)
+
+def categoryProducts(request, slug):
+    category = get_object_or_404(Category, slug=slug)
+    products = Product.objects.filter(category=category)
+
+    count = products.count()
+    context = {
+        'category': category,
+        'products': products,
+        'count': count
+    }
+    return render(request, 'list-category.html', context)
+
+def categorys(request):
+    all_category = Category.objects.all()
+    return {'all_category': all_category}
