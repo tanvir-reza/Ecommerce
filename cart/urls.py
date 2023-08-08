62c13f8e2e3f80b40635d1ec25fbd4ea8725e96e
Md Tanvir Reza <tanvir.reza.abc@gmail.com>
2023-08-08 19:59:57 +0600

    backup

--- cart/urls.py
+++ cart/urls.py
@@ -5,7 +5,7 @@ from django.contrib.staticfiles.urls import staticfiles_urlpatterns, static
 
 urlpatterns = [
     path('', cart_summery, name='cart-summery'),
-    path('add/', cart_add, name='cart-add'),
+    path('add-cart/', cart_add, name='cart-add'),
     path('update/', cart_update, name='cart_update'),
     path('delete', cart_delete, name='cart_delete'),
 ]
